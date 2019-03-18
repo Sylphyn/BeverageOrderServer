a615e7538d315a6d80ea412e9bc3fb9d40718c0c
Sylphyn <lightkirin123@gmail.com>
2019-03-18 21:03:23 +0700

    Beverage Order first commit

---
+++ app/src/main/java/com/example/namhuan/beverageorderserver/Common/Common.java
@@ -0,0 +1,7 @@
+package com.example.namhuan.beverageorderserver.Common;
+
+import com.example.namhuan.beverageorderserver.Model.User;
+
+public class Common {
+    public static User currentUser;
+}
